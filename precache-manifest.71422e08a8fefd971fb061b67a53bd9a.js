89841fe74898d9629d7c77152eddc930f624df50
Benoit <benoit.lacasa@laposte.net>
2019-11-03 12:16:34 +0100

    prod

---
+++ precache-manifest.71422e08a8fefd971fb061b67a53bd9a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601b4b856722293ddd10",
+    "url": "/css/app.0e03bed1.css"
+  },
+  {
+    "revision": "153053d366bf640c838c",
+    "url": "/css/chunk-vendors.7981fadf.css"
+  },
+  {
+    "revision": "b06f31df4d947cf578dfdcac9cf920ba",
+    "url": "/img/annuler.b06f31df.png"
+  },
+  {
+    "revision": "ad3c9f7ef6cf5ed98eb780fe938b09f7",
+    "url": "/img/author.ad3c9f7e.png"
+  },
+  {
+    "revision": "e0c7c5ce1557923ea65a7a701a10d309",
+    "url": "/img/duration.e0c7c5ce.png"
+  },
+  {
+    "revision": "be94d2f305c4630d1c571099da626812",
+    "url": "/img/filter.be94d2f3.png"
+  },
+  {
+    "revision": "c6083c0fd678cf64533aed40e8cce1fd",
+    "url": "/img/gameplayed.c6083c0f.png"
+  },
+  {
+    "revision": "180faa1413e638ad5aa0f713a4a60b6c",
+    "url": "/img/illustrator.180faa14.png"
+  },
+  {
+    "revision": "21d07ade94d472415c41bbbb0ba38520",
+    "url": "/img/player.21d07ade.png"
+  },
+  {
+    "revision": "7d1a839d4a9cccd39f3c928de4cc0741",
+    "url": "/img/save.7d1a839d.png"
+  },
+  {
+    "revision": "fe75ec6341459efb903db0c9dd07592c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "601b4b856722293ddd10",
+    "url": "/js/app.16d61dbd.js"
+  },
+  {
+    "revision": "153053d366bf640c838c",
+    "url": "/js/chunk-vendors.3f1e1449.js"
+  },
+  {
+    "revision": "9d78288cfcd3f25994bf4d1830cdf8a1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
