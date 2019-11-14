5f6344ca55057d25962996b3d0cc24fa3ba93ecb
Benoit <benoit.lacasa@laposte.net>
2019-11-14 08:11:53 +0100

    config

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   assetsDir: "./",
-  publicPath: process.env.NODE_ENV === "production" ? "/my-bgg-list/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/my-bgg-list/" : "/",
+  pwa: {
+    name: "My BGG List",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "./registerServiceWorker.js"
+      // ...other Workbox options...
+    }
+  }
 };
