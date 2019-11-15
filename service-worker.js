0fc17ef7895aa96736ac0700932ec403760b05b6
Benoit <benoit.lacasa@laposte.net>
2019-11-15 13:19:43 +0100

    sw

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,7 @@ const RUNTIME = "runtime";
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   "index.html",
-  "./", // Alias for index.html
-  "./css/*.css",
-  "./js*.js"
+  "./" // Alias for index.html
 ];
 
 // The install handler takes care of precaching the resources we always need.
