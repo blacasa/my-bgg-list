3c3c4d26be920b758436877df894ac8152049b14
Benoit <benoit.lacasa@laposte.net>
2019-11-15 08:27:38 +0100

    sw

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,7 +3,7 @@
 import { register } from "register-service-worker";
 
 //if (process.env.NODE_ENV === 'production') {
-register("./src/registerServiceWorker.js", {
+register("./src/service-worker.js", {
   ready() {
     console.log(
       "App is being served from cache by a service worker.\n" +
@@ -31,25 +31,5 @@ register("./src/registerServiceWorker.js", {
     console.error("Error during service worker registration:", error);
   }
 });
-self.addEventListener("fetch", event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
 
-        return caches.open("runtime").then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
-});
 //}
